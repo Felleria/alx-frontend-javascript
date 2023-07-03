39ae7c71dea5c2e2cb72b9a5344312e2819b7359
Felleria <tellytabbie@gmail.com>
2023-07-04 00:49:46 +0300

    11. Weak link data structure

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,26 +1,8 @@
-// Create a new instance of WeakMap
-const weakMap = new WeakMap();
-
-// Export the weakMap
-export { weakMap };
-
-// Function to query API
+export const weakMap = new WeakMap();
 export function queryAPI(endpoint) {
-  // Get the count for the current endpoint from the weakMap
-  let count = weakMap.get(endpoint) || 0;
-
-  // Increment the count
-  count++;
-
-  // Update the count for the endpoint in the weakMap
-  weakMap.set(endpoint, count);
-
-  // Check if the count exceeds or equals 5
-  if (count >= 5) {
-    throw new Error("Endpoint load is high.");
+  if (!weakMap.has(endpoint)) {
+    weakMap.set(endpoint, 0);
   }
-
-  // Perform the API query
-  // ...
-  // Rest of the code for querying the API
+  weakMap.set(endpoint, weakMap.get(endpoint) + 1);
+  if (weakMap.get(endpoint) >= 5) throw Error('Endpoint load is high');
 }
